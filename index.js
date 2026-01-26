f9e2fb26ba94956f92c041897930419431d46e89
Deadlyranger <muditkubrey@gmail.com>
2026-01-26 23:55:00 +0530

    frontend addon

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // ✅ CORS CONFIG (VERY IMPORTANT)
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true
   })
 );
